976e4acb7b0a12692e7649d677fc04494bc9ae77
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2026-07-20 17:11:28 -0700

    feat(eslint): disable unsafe TypeScript rules for improved flexibility

--- eslint.config.js
+++ eslint.config.js
@@ -48,6 +48,10 @@ export default tseslint.config(
         { selector: ["objectLiteralProperty", "typeProperty"], format: null },
         { selector: "import", format: null },
       ],
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
     },
   },
   {
